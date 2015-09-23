24a0bf4803055f485ea8a38c2b7e564d39ab213e
Bill Pond <william.e.pond@gmail.com>
2015-09-23 11:35:40 -0400

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                   "css/base.css": "less/base.less" // destination file and source file
+                }
+            }
+        },
+        watch: {
+            express: {
+                files:  [ '**/*.js' ],
+                tasks:  [ 'express:dev' ],
+                options: {
+                spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+                }
+            }, 
+            styles: {
+                files: ['less/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+    grunt.registerTask('default', ['watch', 'less']);
+};
